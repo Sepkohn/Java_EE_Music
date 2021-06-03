19a41b0234fa5ec91e9ba64d754fef98923bf4ee
Sepkohn <50259520+Sepkohn@users.noreply.github.com>
2021-06-03 17:07:10 +0200

    Forms

--- TP12-WEB-EJB-PC-EPC-E-donnee/src/main/java/ch/hevs/businessobject/Address.java
+++ TP12-WEB-EJB-PC-EPC-E-donnee/src/main/java/ch/hevs/businessobject/Address.java
@@ -8,30 +8,26 @@ public class Address {
 	private String street;
 	private String city;
 
-	// postalCode
-	public String getCodePostal() {
-	return postalCode;
-	}
-	public void setCodePostal(String codePostal) {
-	this.postalCode = codePostal;
-	}
 
-	// street
+
+	public String getPostalCode() {
+		return postalCode;
+	}
 	public String getStreet() {
-	return street;
+		return street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}
 	public void setStreet(String street) {
-	this.street = street;
-	}
-
-	// city
-	public String getCity() {
-	return city;
+		this.street = street;
 	}
 	public void setCity(String city) {
-	this.city = city;
+		this.city = city;
 	}
-
 	// constructors
 	public Address() {
 	}
